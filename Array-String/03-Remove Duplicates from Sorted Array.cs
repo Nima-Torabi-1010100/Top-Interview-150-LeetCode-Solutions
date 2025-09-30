f5520f29ef9120d80daedb29ab7ac7acd329f28e
Nima Torabi <163128004+Nima-Torabi-1010100@users.noreply.github.com>
2025-09-30 13:51:41 +0330

    Update 03-Remove Duplicates from Sorted Array.cs

--- Array-String/03-Remove Duplicates from Sorted Array.cs
+++ Array-String/03-Remove Duplicates from Sorted Array.cs
@@ -1,15 +1,31 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) 
     {
-            int iw = 1;
-            for (int i = 1; i < nums.Length; i++)
+        int iw = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i-1] != nums[i])
             {
-                if (nums[i-1] != nums[i])
-                {
-                    nums[iw] = nums[i];
-                    iw++;
-                }
+                nums[iw] = nums[i];
+                iw++;
             }
-            return iw;
+        }
+        return iw;
+    }
+    public int RemoveDuplicates(int[] nums) 
+    {
+        int l = nums.Length;
+        int wp = 0;
+        int i = 0;
+        while (i < l)
+        {
+            if (nums[i] != nums[wp])
+            {
+                wp++;
+                nums[wp] = nums[i];
+            }
+            i++;
+        }
+        return wp + 1;
     }
 }
