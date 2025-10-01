65b6635e89bce27420e64ca17c6addb206270352
Nima Torabi <163128004+Nima-Torabi-1010100@users.noreply.github.com>
2025-10-01 23:45:38 +0330

    Update 05-Majority Element.cs

--- Array-String/05-Majority Element.cs
+++ Array-String/05-Majority Element.cs
@@ -1,30 +1,15 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-            int selected = nums[0];
-            int n = nums.Length;
-            int count = 1;
-            for (int i = 1; i < n; i++)
+            int condinate = nums[0];
+            int count = 0;
+
+            foreach (var num in nums)
             {
-                if (selected == nums[i])
-                {
-                    count++;
-                }
-                else
-                {
-                    count--;
-                    if (count == 0)
-                    {
-                        selected = nums[i];
-                        count = 1;
-                    }
-                }
+                if (count == 0)
+                    condinate = num;
+                count += condinate == num ? 1 : -1;
             }
-            count = 0;
-            for (int i = 0; i < n; i++)
-            {
-                if (nums[i] == selected)
-                    count++;
-            }
-            return selected;
+            return condinate;
+        }
     }
 }
